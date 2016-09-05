aa5d9306735aa616d30d0c14174fcbccde39a4bd
Justin.Lin <Justin.Lin@isapsolution.com>
2016-09-05 19:28:37 +0800

    second

--- data.js
+++ data.js
@@ -6,65 +6,41 @@ window.onload = function() {
   bBody.innerHTML=drawTree;
 };
 
+var data = [ {name:1},
+             {name:2,data:[{name:"data 2"}]},
+             {name:3,data:[]}
+           ];
+
 
 function build_file_tree(data) {
         var tree="";
-      
-         
-         for(let i =0, j=data.length;i<j;i++){
+        for(let i =0, len=data.length;i<len;i++){
 
-                tree += "<ul class='tree-folder'>"
-                let tempData = data[i];
-                for(let k in tempData){
-                        console.log(tempData[k]);
-                   if(!isNull(tempData[k])){
-                        if(typeof tempData[k] =="object" && tempData[k].length>0){
-                       
-                          tree+=build_file_tree(tempData[k]);
-                        }else if( tempData[k].length==0 ){
-                          break;
-                        
-                         }else {
-                           tree+="<li>"+tempData[k]+"</i>";
-                                
-                         }
-                   
-
-
-
-                   }
-                        
-
-
-                    
-                      
+             tree += "<ul class='tree-folder'>";
+             
+             for(let key in data[i]){
+                 let tempData=data[i];
+                if(key==="data" && tempData[key].length !==0){
+                 
+                  tree+="<ul>"+build_file_tree(tempData[key])+"</ul>";
                 }
+                tree+="<li>"+tempData[key]+"</li>";
 
-                tree+="</ul>";
-          }   
-        
+             }   
+              tree+="</ul>";
+        }    
         return tree;
-}
+}              
 
 
-function isNull(obj){
-        var isNullValue;
-        for(var key in obj){
-          if(typeof obj[key]=="undefined") {
-                isNullValue=true;
-                break;
-          }else{
-                isNullValue=false};
-                
 
-        }
-        return isNullValue;
-}
 
-var data = [ {name:1},
-             {name:2,data:[{name:"data 2"}]},
-             {name:3,data:null}
-           ];
+
+        
+
+
+
+
 
 
 
